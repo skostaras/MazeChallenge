b847dcab26300a6eb51beef3ca76e66ea2899b9b
Stefanos Kostaras <stefanoskostaras@hotmail.com>
2019-09-09 21:57:31 +0300

    adds one more Maze.class test

--- MazeChallenge/test/com/skostaras/services/MazeTest.java
+++ MazeChallenge/test/com/skostaras/services/MazeTest.java
@@ -4,20 +4,25 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import com.skostaras.entities.Coordinates;
 
 public class MazeTest {
 
 	private static String fileName = "validSolvableMaze1.txt";
 	File mazeTxtFile = new File("resources/" + fileName);
 	Maze maze = new Maze();
+	String mazeString = "__XS_\nXX__X\nXXGXX\n";
 
 	@Test
 	public void mazeFileReaderTest() {
-
 		try {
-			String mazeString = maze.mazeFileReader(mazeTxtFile);
-			assertEquals("__XS_\nXX__X\nXXGXX\n", mazeString.toString());
+			String actualMazeString = maze.mazeFileReader(mazeTxtFile);
+			assertEquals(mazeString, actualMazeString.toString());
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -25,13 +30,27 @@ public class MazeTest {
 
 	@Test
 	public void mazeBuilderTest() {
-
-		String mazeString = "__XS_\nXX__X\nXXGXX\n";
 		String[][] expectedMaze = new String[][] { { "_", "_", "X", "S", "_" }, { "X", "X", "_", "_", "X" },
 				{ "X", "X", "G", "X", "X" } };
 
 		maze.mazeBuilder(mazeString);
 		assertArrayEquals(expectedMaze, maze.maze);
+	}
+
+	@Test
+	public void getExitPathTest() {
+		maze.mazeBuilder(mazeString);
+
+		List<Coordinates> exitPath = new ArrayList<>();
+		exitPath.add(new Coordinates(0, 3));
+		exitPath.add(new Coordinates(1, 3));
+		exitPath.add(new Coordinates(1, 2));
+		exitPath.add(new Coordinates(2, 2));
+
+		String expectedResult = "(0:3 (S)),(1:3),(1:2),(2:2 (G))";
+		StringBuilder actualResult = maze.getExitPath(exitPath);
+
+		assertEquals(expectedResult, actualResult.toString());
 	}
 
 }
